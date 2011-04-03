3c83ead08662173d3f0ce16ab9cc23e0e07e79ae
a2903214 <a2903214@599691cb-4205-09ef-3ef1-462a76ef427f>
2011-04-03 20:04:55 +0000

    首次完成版本

--- trunk/RecImport/src/com/jy/recimport/service/BaseService.java
+++ trunk/RecImport/src/com/jy/recimport/service/BaseService.java
@@ -28,7 +28,7 @@ public abstract class BaseService {
         ConnectionPool.freeConnPool(this.connInfo);
     }
 
-    protected TxnConnection getConnection() throws BaseException {
+    private TxnConnection getConnection() throws BaseException {
         return ConnectionPool.getConnection(connInfo);
     }
 
@@ -71,41 +71,50 @@ public abstract class BaseService {
         }
     }
 
-    public interface DBQueryProcess {
-        final int INT_TYPE = 0;
+    public interface DBExecuteProcess {
 
         void addParameters(PreparedStatement ps) throws SQLException;
 
-        Object resultProcess(ResultSet result) throws SQLException;
+        void resultProcess(ResultSet result) throws SQLException;
+
+        void processGeneratedKeys(ResultSet result) throws SQLException;
+
     }
 
-    protected Object executeSQL(String query, DBQueryProcess process) throws BaseException {
+    public abstract class DBUpdateProcessAdapter implements DBExecuteProcess {
+        public void resultProcess(ResultSet result) throws SQLException {
+
+        }
+    }
+
+    public abstract class DBUpdateNoKeyProcessAdapter implements DBExecuteProcess {
+        public void resultProcess(ResultSet result) throws SQLException {
+
+        }
+
+        public void processGeneratedKeys(ResultSet result) throws SQLException {
+
+        }
+    }
+
+    public abstract class DBQueryProcessAdapter implements DBExecuteProcess {
+        public void processGeneratedKeys(ResultSet result) throws SQLException {
+
+        }
+    }
+
+    protected void executeSQL(String query, String[] columnNames, DBExecuteProcess process)
+            throws BaseException {
         PreparedStatement ps = null;
-        ResultSet result = null;
-        Object ret = null;
         try {
             TxnConnection conn = this.getConnection();
             this.begin();
-            ps = conn.prepareStatement(query);
-            if (process != null) {
-                process.addParameters(ps);
-            }
-            ps.execute();
-            if (process != null) {
-                result = ps.getResultSet();
-                ret = process.resultProcess(result);
-            }
+            ps = conn.prepareStatement(query, columnNames);
+            this.execute(ps, process, true);
             this.commit();
         } catch (Throwable e) {
             this.rollback();
         } finally {
-            if (result != null) {
-                try {
-                    result.close();
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
             if (ps != null) {
                 try {
                     ps.close();
@@ -114,8 +123,60 @@ public abstract class BaseService {
                 }
             }
         }
-        
-        return ret;
+    }
 
+    private void execute(PreparedStatement ps, DBExecuteProcess process, boolean processKey)
+            throws BaseException {
+        ResultSet queryResult = null;
+        ResultSet keyResult = null;
+        try {
+            this.begin();
+            if (process != null) {
+                process.addParameters(ps);
+            }
+            ps.execute();
+            if (process != null) {
+                queryResult = ps.getResultSet();
+                process.resultProcess(queryResult);
+                if (processKey) {
+                    keyResult = ps.getGeneratedKeys();
+                    process.processGeneratedKeys(keyResult);
+                }
+            }
+            this.commit();
+        } catch (Throwable e) {
+            this.rollback();
+            throw new BaseException("执行查询失败！", e);
+        } finally {
+            if (queryResult != null) {
+                try {
+                    queryResult.close();
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    protected void executeSQL(String query, DBExecuteProcess process) throws BaseException {
+        PreparedStatement ps = null;
+        try {
+            TxnConnection conn = this.getConnection();
+            this.begin();
+            ps = conn.prepareStatement(query);
+            this.execute(ps, process, false);
+
+            this.commit();
+        } catch (Throwable e) {
+            this.rollback();
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
